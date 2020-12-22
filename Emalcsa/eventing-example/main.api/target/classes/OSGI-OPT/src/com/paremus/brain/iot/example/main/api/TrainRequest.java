8e6f6062ed511301595a0574386cbafaa35e6c0e
Rui Zhao <rui.zhao@linksfoundation.com>
2020-12-22 15:17:01 +0100

    remove all target folders

--- Emalcsa/eventing-example/main.api/target/classes/OSGI-OPT/src/com/paremus/brain/iot/example/main/api/TrainRequest.java
+++
@@ -1,7 +0,0 @@
-package com.paremus.brain.iot.example.main.api;
-
-import eu.brain.iot.eventing.api.BrainIoTEvent;
-
-public class TrainRequest extends BrainIoTEvent {
-
-}
--- Robot_V2/eventingBusExample/eventing-example/orch.api/target/classes/OSGI-OPT/src/com/paremus/brain/iot/example/orch/api/DoorRequest.java
+++
@@ -1,9 +0,0 @@
-package com.paremus.brain.iot.example.orch.api;
-
-import eu.brain.iot.eventing.api.BrainIoTEvent;
-
-public class DoorRequest extends BrainIoTEvent {
-
-	
-	public String doorevent;
-}
